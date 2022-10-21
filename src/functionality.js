e7bb5be48aa6d762e295a0a796954ca0a4094571
Juanka12 <juancdmh@gmail.com>
2022-10-21 09:50:40 +0200

    Support for imgs and videos

--- src/functionality.js
+++ src/functionality.js
@@ -22,8 +22,41 @@ let projectText = document.getElementById("card-text");
 let projectImg = document.getElementById("card-img");
 let projectLink = document.getElementById("card-link");
 
+let currentMedia = 0;
+let MAX_MEDIA = 1;
+let mediaArray = new Array();
+let mediaContainer = document.getElementById("media-main");
+let detailsLogo = document.getElementById("details-logo");
+let mediaVideo = document.getElementById("media-video");
+
+document.getElementById("details-link").addEventListener('click', function (e) {
+    document.getElementById("details-page").className = "details-page active";
+    fetch('https://raw.githubusercontent.com/Juanka12/PortfolioWeb/main/src/media.json')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(element => {
+                if (element["projectId"] == projectIndex) {
+                    mediaArray.push(element["link"]);
+                }
+            });
+            mediaContainer.src = mediaArray[0];
+            MAX_MEDIA = mediaArray.length-1;
+        })
+        .catch(error => console.log(error));
+});
+document.getElementById("details-close").addEventListener('click', function (e) {
+    document.getElementById("details-page").className = "details-page";
+    mediaArray = new Array();
+    currentMedia = 0;
+    mediaVideo.pause();
+    mediaVideo.currentTime = 0;
+    mediaVideo.className = "media-video hide";
+    mediaContainer.className = "media-main";
+});
+
 AddButtonListeners();
 UpdateCurrentProject();
+AddMediaButtonListeners();
 
 function AddButtonListeners() {
     document.querySelectorAll("div.card-button")[0].addEventListener('click', function () {
@@ -31,7 +64,6 @@ function AddButtonListeners() {
         if (projectIndex < 0) {
             projectIndex = 0;
         }
-        console.log(projectIndex);
         UpdateCurrentProject();
     });
     document.querySelectorAll("div.card-button")[1].addEventListener('click', function () {
@@ -39,7 +71,6 @@ function AddButtonListeners() {
         if (projectIndex > MAX_PROJECTS) {
             projectIndex = MAX_PROJECTS;
         }
-        console.log(projectIndex);
         UpdateCurrentProject();
     });
 }
@@ -50,8 +81,41 @@ function UpdateCurrentProject() {
         .then(data => {
             projectText.innerHTML = data[projectIndex]["text"];
             projectImg.src = data[projectIndex]["img"];
-            projectImg.alt = data[projectIndex]["text"];
+            projectImg.alt = data[projectIndex]["name"];
             projectLink.href = data[projectIndex]["link"];
+            detailsLogo.src = data[projectIndex]["img"];
+            detailsLogo.alt = data[projectIndex]["name"];
         })
         .catch(error => console.log(error));
+}
+
+function AddMediaButtonListeners() {
+    document.querySelectorAll("div.media-button")[0].addEventListener('click', function () {
+        currentMedia --;
+        if (currentMedia < 0) {
+            currentMedia = 0;
+            return;
+        }
+        checkForVideo();
+    });
+    document.querySelectorAll("div.media-button")[1].addEventListener('click', function () {
+        currentMedia ++;
+        if (currentMedia > MAX_MEDIA) {
+            currentMedia = MAX_MEDIA;
+            return;
+        }
+        checkForVideo();
+    });
+}
+
+function checkForVideo() {
+    if (mediaArray[currentMedia].indexOf("download") !== -1) {
+        mediaVideo.src = mediaArray[currentMedia];
+        mediaVideo.className = "media-video";
+        mediaContainer.className = "media-main hide"
+    }else {
+        mediaContainer.src = mediaArray[currentMedia];
+        mediaVideo.className = "media-video hide";
+        mediaContainer.className = "media-main";
+    }
 }
